cf09dd2924e4e4fbfab12456fe44f5f0fa42bfe0
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2022-08-29 13:52:41 +0200

    Create Connect-PgInstance.ps1

---
+++ PostgreSQL/Connect-PgInstance.ps1
@@ -0,0 +1,38 @@
+function Connect-PgInstance {
+    [CmdletBinding()]
+    [OutputType([Devart.Data.PostgreSql.PgSqlConnection])]
+    param (
+        [Parameter(Mandatory)][string]$Instance,
+        [Parameter(Mandatory)][PSCredential]$Credential,
+        [switch]$EnableException
+    )
+
+    if ($Instance -match '^([^:]+):(\d+)$') {
+        $pgHost = $Matches[1]
+        $pgPort = $Matches[2]
+    } else {
+        $pgHost = $Instance
+        $pgPort = 5432
+    }
+
+    Write-Verbose -Message "Creating connection to host [$pgHost] on port [$pgPort]"
+    $connection = [Devart.Data.PostgreSql.PgSqlConnection]::new()
+    $connection.Host = $pgHost
+    $connection.Port = $pgPort
+    $connection.UserId = $Credential.UserName
+    $connection.Password = $Credential.GetNetworkCredential().Password
+
+    try {
+        Write-Verbose -Message "Opening connection"
+        $connection.Open()
+        
+        Write-Verbose -Message "Returning connection object"
+        $connection
+    } catch {
+        if ($EnableException) {
+            throw
+        } else {
+            Write-Warning -Message "Connection could not be opened: $($_.Exception.InnerException.Message)"
+        }
+    }
+}
