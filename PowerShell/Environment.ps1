b6b902a8b98475b96079be5994cc265ffe15cd33
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2022-08-30 11:13:07 +0200

    Update Environment.ps1

--- PowerShell/Environment.ps1
+++ PowerShell/Environment.ps1
@@ -3,10 +3,14 @@ $ErrorActionPreference = 'Stop'
 # The following two variables will be used in other scripts like all the Server.ps1 to run the installation from the client computer.
 
 # The name of the computer where all the database management software will be installed.
-$serverComputerName = 'SQLLAB08'
+if (-not $serverComputerName) {
+    $serverComputerName = 'SQLLAB08'
+}
 
 # The credential of a domain user that has administrative rights on the target server ($serverComputerName).
-$windowsAdminCredential = Get-Credential -Message "Account to connect to target server with CredSSP" -UserName "$env:USERDOMAIN\$env:USERNAME"
+if (-not $windowsAdminCredential) {
+    $windowsAdminCredential = Get-Credential -Message "Account to connect to target server with CredSSP" -UserName "$env:USERDOMAIN\$env:USERNAME"
+}
 
 # Test CredSSP
 try {
