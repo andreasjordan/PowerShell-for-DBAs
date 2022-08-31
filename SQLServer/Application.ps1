cc2a22e01c2a55efb1a8e8ea4e7306daf963538e
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2022-08-31 11:02:46 +0200

    Update Application.ps1

--- SQLServer/Application.ps1
+++ SQLServer/Application.ps1
@@ -4,81 +4,31 @@ Import-Module -Name dbatools  # Install-Module -Name dbatools -Scope CurrentUser
 
 . ..\PowerShell\Environment.ps1
 . ..\PowerShell\Import-Schema.ps1
+. ..\PowerShell\Import-Data.ps1
 
-$instance = "$serverComputerName\SQLEXPRESS"
+$instance = "$EnvironmentServerComputerName\SQLEXPRESS"
 
-$credentialAdmin = Get-Credential -Message $instance -UserName sa             # start123
-$credentialUser  = Get-Credential -Message $instance -UserName stackoverflow  # start456
+# $credentialAdmin = Get-Credential -Message $instance -UserName sa
+$credentialAdmin = [PSCredential]::new('sa', (ConvertTo-SecureString -String $EnvironmentDatabaseAdminPassword -AsPlainText -Force))
+
+# $credentialUser  = Get-Credential -Message $instance -UserName stackoverflow
+$credentialUser = [PSCredential]::new('StackOverflow', (ConvertTo-SecureString -String $EnvironmentDatabaseUserPassword -AsPlainText -Force))
+
 
 $connectionAdmin = Connect-DbaInstance -SqlInstance $instance -SqlCredential $credentialAdmin -NonPooledConnection
 
-$null = Remove-DbaDatabase -SqlInstance $connectionAdmin -Database stackoverflow -Confirm:$false
-$null = Remove-DbaLogin -SqlInstance $connectionAdmin -Login stackoverflow -Confirm:$false
+$null = Remove-DbaDatabase -SqlInstance $connectionAdmin -Database StackOverflow -Confirm:$false
+$null = Remove-DbaLogin -SqlInstance $connectionAdmin -Login StackOverflow -Confirm:$false
 
-$null = New-DbaLogin -SqlInstance $connectionAdmin -Login stackoverflow -SecurePassword $credentialUser.Password
-$null = New-DbaDatabase -SqlInstance $connectionAdmin -Name stackoverflow -Owner stackoverflow
+$null = New-DbaLogin -SqlInstance $connectionAdmin -Login StackOverflow -SecurePassword $credentialUser.Password
+$null = New-DbaDatabase -SqlInstance $connectionAdmin -Name StackOverflow -Owner StackOverflow
 
 $null = $connectionAdmin | Disconnect-DbaInstance
 
 
-$connectionUser = Connect-DbaInstance -SqlInstance $instance -SqlCredential $credentialUser -Database stackoverflow -NonPooledConnection
+$connectionUser = Connect-DbaInstance -SqlInstance $instance -SqlCredential $credentialUser -Database StackOverflow -NonPooledConnection
 
-$schema = Import-Schema -Path ..\PowerShell\Schema.psd1 -DBMS SQLServer
-foreach ($query in $schema) {
-    Invoke-DbaQuery -SqlInstance $connectionUser -Query $query
-}
-
-$data = Get-Content -Path ..\PowerShell\SampleData.json -Encoding UTF8 | ConvertFrom-Json
-$tableNames = $data.PSObject.Properties.Name | Sort-Object
-
-$progressTableParameter = @{ Id = 1 ; Activity = 'Importing tables' }
-$progressTableTotal = $tableNames.Count
-$progressTableCompleted = 0 
-
-foreach ($tableName in $tableNames) {
-    $progressTableParameter.Status = "$progressTableCompleted of $progressTableTotal tables completed"
-    $progressTableParameter.CurrentOperation = "processing table $tableName"
-    Write-Progress @progressTableParameter
-    $progressTableCompleted++
-
-    $insertIntoSql1 = "INSERT INTO $tableName ("
-    $insertIntoSql2 = " VALUES ("
-    $columNames = ($data.$tableName | Select-Object -First 1).PSObject.Properties.Name
-    foreach ($columnName in $columNames) {
-        $insertIntoSql1 += "$columnName, "
-        $insertIntoSql2 += "@$columnName, "
-    }
-    $insertIntoSql1 = $insertIntoSql1.TrimEnd(', ') + ')'
-    $insertIntoSql2 = $insertIntoSql2.TrimEnd(', ') + ')'
-    $insertIntoSql = $insertIntoSql1 + $insertIntoSql2
-
-    $progressRowParameter = @{ Id = 2 ; Activity = 'Importing rows' }
-    $progressRowTotal = $data.$tableName.Count
-    $progressRowCompleted = 0 
-    $progressRowStart = Get-Date
-
-    foreach ($row in $data.$tableName) {
-        if ($progressRowCompleted % 100 -eq 0) {
-            $progressRowParameter.Status = "$progressRowCompleted of $progressRowTotal rows completed"
-            $progressRowParameter.PercentComplete = $progressRowCompleted * 100 / $progressRowTotal
-            if ($progressRowParameter.PercentComplete -gt 0) {
-                $progressRowParameter.SecondsRemaining = ((Get-Date) - $progressRowStart).TotalSeconds / $progressRowParameter.PercentComplete * (100 - $progressRowParameter.PercentComplete)
-                $progressRowParameter.Status += " ($([int]($progressRowCompleted / ((Get-Date) - $progressRowStart).TotalSeconds)) rows per second)"
-            }
-            Write-Progress @progressRowParameter
-        }
-        $progressRowCompleted++
-
-        $parameterValues = @{}
-        foreach ($column in $row.PSObject.Properties) {
-            $parameterValues[$column.Name] = $column.Value
-        }
-        Invoke-DbaQuery -SqlInstance $connectionUser -Query $insertIntoSql -SqlParameter $parameterValues
-    }
-
-    Write-Progress @progressRowParameter -Completed
-}
-
-Write-Progress @progressTableParameter -Completed
+Import-Schema -Path ..\PowerShell\SampleSchema.psd1 -DBMS SQLServer -Connection $connectionUser
+Import-Data -Path ..\PowerShell\SampleData.json -DBMS SQLServer -Connection $connectionUser
 
 $null = $connectionUser | Disconnect-DbaInstance
