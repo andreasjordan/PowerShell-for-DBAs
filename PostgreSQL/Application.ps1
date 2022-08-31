8b6fa96508c76e61e698402294cc268dd0cdad24
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2022-08-31 14:09:15 +0200

    Update Application.ps1

--- PostgreSQL/Application.ps1
+++ PostgreSQL/Application.ps1
@@ -1,7 +1,10 @@
 $ErrorActionPreference = 'Stop'
 
-# This script uses the variable $serverComputerName that was set in Environment.ps1.
-# This script uses Connect-PgInstance.ps1, Invoke-PgQuery.ps1 and Import-Schema.ps1.
+. ..\PowerShell\Environment.ps1
+. ..\PowerShell\Import-Schema.ps1
+. ..\PowerShell\Import-Data.ps1
+. .\Connect-PgInstance.ps1
+. .\Invoke-PgQuery.ps1
 
 try {
     Add-Type -Path 'C:\Program Files (x86)\Devart\dotConnect\PostgreSQL\Devart.Data.PostgreSql.dll'
@@ -9,12 +12,16 @@ try {
     $ex = $_
     $ex.Exception.Message
     $ex.Exception.LoaderExceptions
+    throw "Adding type failed"
 }
 
-$instance = $serverComputerName
+$instance = $EnvironmentServerComputerName
 
-$credentialAdmin = Get-Credential -Message $instance -UserName postgres       # start123
-$credentialUser  = Get-Credential -Message $instance -UserName stackoverflow  # start456
+# $credentialAdmin = Get-Credential -Message $instance -UserName postgres
+$credentialAdmin = [PSCredential]::new('postgres', (ConvertTo-SecureString -String $EnvironmentDatabaseAdminPassword -AsPlainText -Force))
+
+# $credentialUser  = Get-Credential -Message $instance -UserName stackoverflow
+$credentialUser = [PSCredential]::new('stackoverflow', (ConvertTo-SecureString -String $EnvironmentDatabaseUserPassword -AsPlainText -Force))
 
 
 $connectionAdmin = Connect-PgInstance -Instance $instance -Credential $credentialAdmin
@@ -31,10 +38,8 @@ $connectionAdmin.Dispose()
 
 $connectionUser = Connect-PgInstance -Instance $instance -Credential $credentialUser -Database stackoverflow
 
-$schema = Import-Schema -Path \\fs\Skripte\PowerShell-for-DBAs\PowerShell\Schema.psd1 -DBMS PostgreSQL
-foreach ($query in $schema) {
-    Invoke-PgQuery -Connection $connectionUser -Query $query
-}
+Import-Schema -Path ..\PowerShell\SampleSchema.psd1 -DBMS PostgreSQL -Connection $connectionUser
+Import-Data -Path ..\PowerShell\SampleData.json -DBMS PostgreSQL -Connection $connectionUser
 
 $connectionUser.Close()
 $connectionUser.Dispose()
