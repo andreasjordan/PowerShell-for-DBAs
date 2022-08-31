95f08e1c8544b4ba94eed6345e38003852536bbd
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2022-08-31 11:01:07 +0200

    Create Import-Data.ps1

---
+++ PowerShell/Import-Data.ps1
@@ -0,0 +1,98 @@
+function Import-Data {
+    [CmdletBinding()]
+    [OutputType([System.Void])]
+    param (
+        [Parameter(Mandatory)][string]$Path,
+        [Parameter(Mandatory)][ValidateSet("SQLServer", "Oracle", "PostgreSQL", "MySQL", "Db2", "Informix")][string]$DBMS,
+        [Parameter(Mandatory)][object]$Connection,
+        [switch]$EnableException
+    )
+
+    try {
+        Write-Verbose -Message "Importing JSON from $Path"
+        $data = Get-Content -Path $Path -Encoding UTF8 | ConvertFrom-Json
+
+        $tableNames = $data.PSObject.Properties.Name | Sort-Object
+
+        $bindParameterSymbol = ':'
+        if ($DBMS -eq 'SQLServer') {
+            $bindParameterSymbol = '@'
+        }
+
+        $progressTableParameter = @{ Id = 1 ; Activity = 'Importing tables' }
+        $progressTableTotal = $tableNames.Count
+        $progressTableCompleted = 0 
+
+        foreach ($tableName in $tableNames) {
+            Write-Verbose -Message "Processing table $tableName"
+
+            $progressTableParameter.Status = "$progressTableCompleted of $progressTableTotal tables completed"
+            $progressTableParameter.CurrentOperation = "processing table $tableName"
+            Write-Progress @progressTableParameter
+            $progressTableCompleted++
+
+            $insertIntoSql1 = "INSERT INTO $tableName ("
+            $insertIntoSql2 = " VALUES ("
+            $columNames = ($data.$tableName | Select-Object -First 1).PSObject.Properties.Name
+            foreach ($columnName in $columNames) {
+                $insertIntoSql1 += "$columnName, "
+                $insertIntoSql2 += "$bindParameterSymbol$columnName, "
+            }
+            $insertIntoSql1 = $insertIntoSql1.TrimEnd(', ') + ')'
+            $insertIntoSql2 = $insertIntoSql2.TrimEnd(', ') + ')'
+            $insertIntoSql = $insertIntoSql1 + $insertIntoSql2
+
+            $progressRowParameter = @{ Id = 2 ; Activity = 'Importing rows' }
+            $progressRowTotal = $data.$tableName.Count
+            $progressRowCompleted = 0 
+            $progressRowStart = Get-Date
+
+            foreach ($row in $data.$tableName) {
+                if ($progressRowCompleted % 100 -eq 0) {
+                    $progressRowParameter.Status = "$progressRowCompleted of $progressRowTotal rows completed"
+                    $progressRowParameter.PercentComplete = $progressRowCompleted * 100 / $progressRowTotal
+                    if ($progressRowParameter.PercentComplete -gt 0) {
+                        $progressRowParameter.SecondsRemaining = ((Get-Date) - $progressRowStart).TotalSeconds / $progressRowParameter.PercentComplete * (100 - $progressRowParameter.PercentComplete)
+                        $progressRowParameter.Status += " ($([int]($progressRowCompleted / ((Get-Date) - $progressRowStart).TotalSeconds)) rows per second)"
+                    }
+                    Write-Progress @progressRowParameter
+                }
+                $progressRowCompleted++
+
+                $parameterValues = @{}
+                foreach ($column in $row.PSObject.Properties) {
+                    $parameterValues[$column.Name] = $column.Value
+                }
+
+                switch ($DBMS) {
+                    SQLServer {
+                        $null = Invoke-DbaQuery -SqlInstance $Connection -Query $insertIntoSql -SqlParameter $parameterValues -EnableException
+                    }
+                    Oracle {
+                        $null = Invoke-OraQuery -Connection $Connection -Query $insertIntoSql -ParameterValues $parameterValues -EnableException
+                    }
+                    PostgreSQL {
+                        $null = Invoke-PgQuery -Connection $Connection -Query $insertIntoSql -ParameterValues $parameterValues -EnableException
+                    }
+                    MySQL {
+                        $null = Invoke-MyQuery -Connection $Connection -Query $insertIntoSql -ParameterValues $parameterValues -EnableException
+                    }
+                    Db2 {
+                        $null = Invoke-Db2Query -Connection $Connection -Query $insertIntoSql -ParameterValues $parameterValues -EnableException
+                    }
+                    Informix {
+                        $null = Invoke-IfxQuery -Connection $Connection -Query $insertIntoSql -ParameterValues $parameterValues -EnableException
+                    }
+                }
+            }
+            Write-Progress @progressRowParameter -Completed
+        }
+        Write-Progress @progressTableParameter -Completed
+    } catch {
+        if ($EnableException) {
+            throw
+        } else {
+            Write-Warning -Message "Data could not be imported: $_"
+        }
+    }
+}
