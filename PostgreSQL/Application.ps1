f5b3ad63bf6900c300160e72562eb6ad1fb28285
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2022-09-06 11:56:04 +0200

    Update Application.ps1

--- PostgreSQL/Application.ps1
+++ PostgreSQL/Application.ps1
@@ -3,16 +3,20 @@ $ErrorActionPreference = 'Stop'
 . ..\PowerShell\Environment.ps1
 . ..\PowerShell\Import-Schema.ps1
 . ..\PowerShell\Import-Data.ps1
-. .\Connect-PgInstance.ps1
-. .\Invoke-PgQuery.ps1
 
-try {
-    Add-Type -Path 'C:\Program Files (x86)\Devart\dotConnect\PostgreSQL\Devart.Data.PostgreSql.dll'
-} catch {
-    $ex = $_
-    $ex.Exception.Message
-    $ex.Exception.LoaderExceptions
-    throw "Adding type failed"
+if (-not $Env:POSTGRESQL_DLL) {
+    throw 'Environment variable MYSQL_DLL not set'
+}
+if (-not (Test-Path -Path $Env:POSTGRESQL_DLL)) {
+    throw "Environment variable MYSQL_DLL not set correctly, file [$Env:POSTGRESQL_DLL] not found"
+}
+Add-Type -Path $Env:POSTGRESQL_DLL
+if ($Env:POSTGRESQL_DLL -match 'Devart') {
+    . .\Connect-PgInstance_Devart.ps1
+    . .\Invoke-PgQuery_Devart.ps1
+} else {
+    . .\Connect-PgInstance.ps1
+    . .\Invoke-PgQuery.ps1
 }
 
 $instance = $EnvironmentServerComputerName
@@ -37,9 +41,12 @@ $connectionAdmin.Dispose()
 
 
 $connectionUser = Connect-PgInstance -Instance $instance -Credential $credentialUser -Database stackoverflow
-
 Import-Schema -Path ..\PowerShell\SampleSchema.psd1 -DBMS PostgreSQL -Connection $connectionUser
+$start = Get-Date
 Import-Data -Path ..\PowerShell\SampleData.json -DBMS PostgreSQL -Connection $connectionUser
+$duration = (Get-Date) - $start
+Write-Host "Data import finished in $($duration.TotalSeconds) seconds"
 
 $connectionUser.Close()
 $connectionUser.Dispose()
+
