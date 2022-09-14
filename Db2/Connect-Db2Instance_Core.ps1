9a97e483d14814390fc97328e824d498be2f532c
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2022-09-14 11:42:59 +0200

    Create Connect-Db2Instance_Core.ps1

---
+++ Db2/Connect-Db2Instance_Core.ps1
@@ -0,0 +1,42 @@
+function Connect-Db2Instance {
+    [CmdletBinding()]
+    [OutputType([IBM.Data.DB2.Core.DB2Connection])]
+    param (
+        [Parameter(Mandatory)][string]$Instance,
+        [Parameter(Mandatory)][PSCredential]$Credential,
+        [Parameter(Mandatory)][string]$Database,
+        [switch]$PooledConnection,
+        [switch]$EnableException
+    )
+
+    Write-Verbose -Message "Creating connection to server [$Instance] and database [$Database]"
+
+    $csb = [IBM.Data.DB2.Core.DB2ConnectionStringBuilder]::new()
+    $csb.Server = $Instance
+    $csb.Database = $Database
+    $csb.UserID = $Credential.UserName
+    $csb.Password = $Credential.GetNetworkCredential().Password
+    if ($PooledConnection) {
+        Write-Verbose -Message "Using connection pooling"
+        $csb.Pooling = $true
+    } else {
+        Write-Verbose -Message "Disabling connection pooling"
+        $csb.Pooling = $false
+    }
+
+    $connection = [IBM.Data.DB2.Core.DB2Connection]::new($csb.ConnectionString)
+
+    try {
+        Write-Verbose -Message "Opening connection"
+        $connection.Open()
+        
+        Write-Verbose -Message "Returning connection object"
+        $connection
+    } catch {
+        if ($EnableException) {
+            throw
+        } else {
+            Write-Warning -Message "Connection could not be opened: $($_.Exception.InnerException.Message)"
+        }
+    }
+}
