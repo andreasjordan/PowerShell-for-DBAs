17d6c8e2527e1d4ca33845fdeb9611389a555701
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2022-09-20 16:10:39 +0200

    Update Application.ps1

--- Informix/Application.ps1
+++ Informix/Application.ps1
@@ -10,12 +10,25 @@ if (-not $Env:INFORMIX_DLL) {
 if (-not (Test-Path -Path $Env:INFORMIX_DLL)) {
     throw "Environment variable INFORMIX_DLL not set correctly, file [$Env:INFORMIX_DLL] not found"
 }
-if ($Env:INFORMIX_DLL -match 'IBM.Data.DB2') {
+if ($Env:INFORMIX_DLL -match 'IBM.Data.DB2.dll') {
     try { Add-Type -Path $Env:INFORMIX_DLL } catch { }
 
     . .\Connect-IfxInstance_Db2.ps1
     . .\Invoke-IfxQuery_Db2.ps1
     
+    $instance = "$($EnvironmentServerComputerName):9089"
+} elseif ($Env:INFORMIX_DLL -match 'IBM.Data.DB2.Core.dll') {
+    # Test for NuGet package and change $Env:PATH
+    if ($Env:INFORMIX_DLL -match 'lib\\[^\\]+\\IBM\.Data\.Db2(\.Core)?\.dll') {
+        $path = $Env:DB2_DLL -replace 'lib\\[^\\]+\\IBM\.Data\.Db2(\.Core)?\.dll', 'buildTransitive\clidriver\bin'
+        $Env:PATH = "$Env:PATH;$path"
+    }
+
+    Add-Type -Path $Env:INFORMIX_DLL
+
+    . .\Connect-IfxInstance_Db2_Core.ps1
+    . .\Invoke-IfxQuery_Db2_Core.ps1
+
     $instance = "$($EnvironmentServerComputerName):9089"
 } else {
     Add-Type -Path $Env:INFORMIX_DLL
