b3eaf5347ffc52959bd894136f6d0d8963e717bf
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2022-09-20 16:53:56 +0200

    Update Application.ps1

--- Informix/Application.ps1
+++ Informix/Application.ps1
@@ -23,8 +23,8 @@ if ($Env:INFORMIX_DLL -match 'IBM.Data.DB2.dll') {
         $path = $Env:INFORMIX_DLL -replace 'lib\\[^\\]+\\IBM\.Data\.Db2(\.Core)?\.dll', 'buildTransitive\clidriver\bin'
         $Env:PATH = "$Env:PATH;$path"
     }
-    if ($Env:INFORMIX_DLL -match 'lib/[^/]+/IBM\.Data\.Db2(\.Core)?\.dll') {
-        $path = $Env:INFORMIX_DLL -replace 'lib/[^/]+/IBM\.Data\.Db2(\.Core)?\.dll', 'buildTransitive/clidriver/lib'
+    if ($Env:INFORMIX_DLL -match 'lib/[^/]+/IBM\.Data\.Db2\.Core-lnx\.dll') {
+        $path = $Env:INFORMIX_DLL -replace 'lib/[^/]+/IBM\.Data\.Db2\.Core-lnx\.dll', 'buildTransitive/clidriver/lib'
         $Env:PATH = "$Env:PATH:$path"
     }
 
