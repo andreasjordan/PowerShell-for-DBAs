51c70c9ce5bed50afb5d8183b79c7258cca4892d
Andreas Jordan <anj@ordix.de>
2022-10-03 15:51:31 +0200

    support docker

--- SQLServer/Application.ps1
+++ SQLServer/Application.ps1
@@ -6,32 +6,25 @@ Import-Module -Name dbatools  # Install-Module -Name dbatools -Scope CurrentUser
 . ..\PowerShell\Import-Schema.ps1
 . ..\PowerShell\Import-Data.ps1
 
-$instance = "$EnvironmentServerComputerName\SQLEXPRESS"
+if ($EnvironmentServerComputerName -in 'localhost', 'SQLServer-1') {
+    $instance = $EnvironmentServerComputerName
+} else {
+    $instance = "$EnvironmentServerComputerName\SQLEXPRESS"
+}
 
-# $credentialAdmin = Get-Credential -Message $instance -UserName sa
-$credentialAdmin = [PSCredential]::new('sa', (ConvertTo-SecureString -String $EnvironmentDatabaseAdminPassword -AsPlainText -Force))
+try {
+    # $credentialUser = Get-Credential -Message $instance -UserName $EnvironmentDatabaseUserName
+    $credential = [PSCredential]::new($EnvironmentDatabaseUserName, (ConvertTo-SecureString -String $EnvironmentDatabaseUserPassword -AsPlainText -Force))
+    $connection = Connect-DbaInstance -SqlInstance $instance -SqlCredential $credential -Database StackOverflow -NonPooledConnection
 
-# $credentialUser  = Get-Credential -Message $instance -UserName StackOverflow
-$credentialUser = [PSCredential]::new('StackOverflow', (ConvertTo-SecureString -String $EnvironmentDatabaseUserPassword -AsPlainText -Force))
+    Import-Schema -Path ..\PowerShell\SampleSchema.psd1 -DBMS SQLServer -Connection $connection -EnableException
+    $start = Get-Date
+    Import-Data -Path ..\PowerShell\SampleData.json -DBMS SQLServer -Connection $connection -EnableException
+    $duration = (Get-Date) - $start
 
+    $null = $connection | Disconnect-DbaInstance
 
-$connectionAdmin = Connect-DbaInstance -SqlInstance $instance -SqlCredential $credentialAdmin -NonPooledConnection
-
-$null = Remove-DbaDatabase -SqlInstance $connectionAdmin -Database StackOverflow -Confirm:$false
-$null = Remove-DbaLogin -SqlInstance $connectionAdmin -Login StackOverflow -Confirm:$false
-
-$null = New-DbaLogin -SqlInstance $connectionAdmin -Login StackOverflow -SecurePassword $credentialUser.Password
-$null = New-DbaDatabase -SqlInstance $connectionAdmin -Name StackOverflow -Owner StackOverflow
-
-$null = $connectionAdmin | Disconnect-DbaInstance
-
-
-$connectionUser = Connect-DbaInstance -SqlInstance $instance -SqlCredential $credentialUser -Database StackOverflow -NonPooledConnection
-
-Import-Schema -Path ..\PowerShell\SampleSchema.psd1 -DBMS SQLServer -Connection $connectionUser
-$start = Get-Date
-Import-Data -Path ..\PowerShell\SampleData.json -DBMS SQLServer -Connection $connectionUser
-$duration = (Get-Date) - $start
-Write-Host "Data import finished in $($duration.TotalSeconds) seconds"
-
-$null = $connectionUser | Disconnect-DbaInstance
+    Write-Host "Data import to $EnvironmentServerComputerName finished in $($duration.TotalSeconds) seconds"
+} catch {
+    Write-Host "Data import to $EnvironmentServerComputerName failed: $_"
+}
