0e0fc2f73410d88469403466eab68bdb0347098c
andreasjordan <anj@ordix.de>
2022-10-19 18:40:51 +0200

    Be able to use this script to reclone git

--- WSL2/07_setup_git.sh
+++ WSL2/07_setup_git.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-mkdir ~/GitHub && \
+# This script can be run again later to refresh the repository.
+[ -d ~/GitHub/PowerShell-for-DBAs ] && rm -rf ~/GitHub/PowerShell-for-DBAs
+
+# On first run, create the folder.
+[ ! -d ~/GitHub ] && mkdir ~/GitHub
+
+# Clone the repository.
 cd ~/GitHub && \
 git clone https://github.com/andreasjordan/PowerShell-for-DBAs.git && \
 echo 'OK'
