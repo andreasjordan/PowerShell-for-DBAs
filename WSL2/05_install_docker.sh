7a1b55f9f06698de50a24fa73f7f6dd8c556d9d7
andreasjordan <anj@ordix.de>
2022-10-20 15:25:58 +0200

    create directory because missing in 20.04

--- WSL2/05_install_docker.sh
+++ WSL2/05_install_docker.sh
@@ -3,6 +3,7 @@
 # https://docs.docker.com/engine/install/ubuntu/
 
 apt-get install -y ca-certificates curl gnupg2 lsb-release && \
+mkdir -p /etc/apt/keyrings && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list && \
 apt update && \
