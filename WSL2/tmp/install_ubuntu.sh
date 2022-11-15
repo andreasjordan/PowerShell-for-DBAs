b2b3017a3778631f65401731e50beb0256f9eab9
Andreas Jordan <66946165+andreasjordan@users.noreply.github.com>
2022-11-15 19:56:29 +0100

    Update and rename install_ubunto.sh to install_ubuntu.sh

--- WSL2/tmp/install_ubuntu.sh
+++ WSL2/tmp/install_ubuntu.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# 02_update_packages.sh
+# update packages
 apt update && \
 apt -y upgrade && \
-# 03_install_pwsh.sh
+# install ssh
+apt-get install -y openssh-server && \
+# install powershell
 apt-get install -y wget apt-transport-https software-properties-common && \
 wget -q -O /tmp/packages-microsoft-prod.deb "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb" && \
 dpkg -i /tmp/packages-microsoft-prod.deb && \
 apt-get update && \
 apt-get install -y powershell && \
-# 05_install_docker.sh
+# install docker
 apt-get install -y ca-certificates curl gnupg2 lsb-release && \
 mkdir -p /etc/apt/keyrings && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg && \
@@ -18,7 +20,4 @@ apt update && \
 apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin && \
 update-alternatives --set iptables /usr/sbin/iptables-legacy && \
 service docker start && \
-# 06_setup_docker.sh
-docker pull mcr.microsoft.com/mssql/server:2019-latest && \
-docker pull container-registry.oracle.com/database/express:latest && \
 echo 'OK'
