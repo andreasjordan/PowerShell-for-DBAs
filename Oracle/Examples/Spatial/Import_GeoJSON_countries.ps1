d3e1f8e3168aa8aef56c026778aa0aa9c57482c3
andreasjordan <anj@ordix.de>
2023-01-11 13:46:22 +0100

    Limit decimal places

--- Oracle/Examples/Spatial/Import_GeoJSON_countries.ps1
+++ Oracle/Examples/Spatial/Import_GeoJSON_countries.ps1
@@ -19,7 +19,7 @@ foreach ($feature in $geoJSON.features) {
         ParameterValues = @{
             name     = $feature.properties.ADMIN
             iso      = $feature.properties.ISO_A3
-            geometry = $feature.geometry | ConvertTo-Json -Depth 4
+            geometry = ($feature.geometry | ConvertTo-Json -Depth 4 -Compress) -replace '\.(\d{10})\d+', '.$1'
         }
     }
     try {
@@ -30,10 +30,6 @@ foreach ($feature in $geoJSON.features) {
 }
 
 <#
-Some info on why I don't use -Compress with ConvertTo-Json in line 22:
-ConvertFrom-Json (which is used by Invoke-RestMethod) works different on PowerShell 5.1 and 7.3. 
-It returns Decimal on 5.1 and Double on 7.3 - so on PowerShell 7.3 some values are rounded.
-I still don't know why exactly, but with the rounded values, the data for Kazakhstan produces an "invalid" (only for Oracle invalid) string when -Compress is used. 
-But if I send an uncompressed JSON string to Oracle, everything works with the rounded values on PowerShell 7.3
-See also: https://community.oracle.com/tech/developers/discussion/comment/16859191
+Some info on why I have to limit the number of decimal places in line 22:
+https://community.oracle.com/tech/developers/discussion/comment/16859191
 #>
